abccdff0419b2f507d8731cd868d11940e721e21
Jose Mario Gutierrez <jogutierrez@H7689.ad.hugeinc.com>
2018-11-14 09:02:58 -0500

    Added functions to get blood compatibility

--- blood-donation.js
+++ blood-donation.js
@@ -1,3 +1,7 @@
+const axios = require("axios");
+
+const canDonateTo = {};
+
 const chart = {
     "O-": ["AB+","AB-","A+","A-","B+","B-","O+","O-"],
     "O+": ["AB+","A+","B+","O+"],
@@ -7,4 +11,46 @@ const chart = {
     "A+": ["AB+","A+"],
     "AB-": ["AB+","AB-"],
     "AB+": ["AB+"],
+}
+
+axios.get('https://jsconf.admios.com/blood-types')
+    .then((resp) => {
+        answer = getBloodCompatibility(resp.data)
+        console.log(resp.data);
+        console.log(answer);
+        
+    });
+
+function checkIfCanDonate(chart, donorBlood, doneeBlood){
+
+    const donorChart = chart[donorBlood];
+
+    for (let aceptedDoneeBlood of donorChart) {
+        if(aceptedDoneeBlood == doneeBlood){
+            return true;
+            break;
+        }
+    }
+}
+
+function getBloodCompatibility(donatorsList){
+    
+    for (let donor in donatorsList) {
+        if (donatorsList.hasOwnProperty(donor)) {
+            
+            const doneesList = []
+            
+            for (let donee in donatorsList) {
+                if(donor != donee){
+                    if(checkIfCanDonate(chart, donatorsList[donor], donatorsList[donee])){
+                        doneesList.push(donee)
+                    }
+                }
+            }
+            
+            canDonateTo[donor] = doneesList;
+        }
+    }
+    
+    return canDonateTo;
 }
